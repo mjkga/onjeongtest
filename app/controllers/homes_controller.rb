074fa876ce6a6b83f8d77c375535709f1058fa16
aeri choi <aeri.choi715@likelion.org>
2019-08-23 13:22:17 +0000

    commit before migration

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -9,9 +9,9 @@ class HomesController < ApplicationController
   
   def create
     post = Cat_feed.new
-    post.name = params[:v-cat-feed-name]
-    post.info = params[:v-cat-feed-info]
-    post.status = params[:v-cat-feed-status]
-    post.show = params[:v-cat-feed-show]
+    post.name = params[:'v-cat-feed-name']
+    post.info = params[:'v-cat-feed-info']
+    post.status = params[:'v-cat-feed-status']
+    post.show = params[:'v-cat-feed-show']
   end
 end
